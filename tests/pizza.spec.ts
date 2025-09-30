48fdb89782dd76fc11fbcb741b6ed88526cd1595
MrMintCode <Ruben.Matos.imp@gmail.com>
2025-09-30 16:39:44 -0600

    Progress in making tests.

--- tests/pizza.spec.ts
+++ tests/pizza.spec.ts
@@ -1,8 +1,45 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from 'playwright-test-coverage';
 
-test('has title', async ({ page }) => {
+test('home page loads', async ({ page }) => {
   await page.goto('http://localhost:5173/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle('JWT Pizza');
+});
+
+test('login', async ({ page }) => {
+  await page.route('*/**/api/auth', async (route) => {
+    const loginReq = { email: 'd@jwt.com', password: 'a' };
+    const loginRes = {
+      user: {
+        id: 3,
+        name: 'Kai Chen',
+        email: 'd@jwt.com',
+        roles: [{ role: 'diner' }],
+      },
+      token: 'abcdef',
+    };
+    expect(route.request().method()).toBe('PUT');
+    expect(route.request().postDataJSON()).toMatchObject(loginReq);
+    await route.fulfill({ json: loginRes });
+  });
+
+
+  await page.goto('http://localhost:5173/');  
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).fill('a@jwt.com');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('admin');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByRole('link', { name: '常' }).click();
+  await expect(page.getByRole('heading')).toContainText('Your pizza kitchen');');
+
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).fill('a@jwt.com');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('admin');
+  await page.getByRole('button', { name: 'Login' }).click();
+
+
 });
