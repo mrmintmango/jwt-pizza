9243edd23e7976d08238e5ea6a2c77279580df6d
MrMintCode <Ruben.Matos.imp@gmail.com>
2025-11-18 18:18:03 -0700

    added load test grafana script

---
+++ loadTests/loginAndOrder.js
@@ -0,0 +1,204 @@
+import { sleep, check, fail } from 'k6'
+import http from 'k6/http'
+import jsonpath from 'https://jslib.k6.io/jsonpath/1.0.2/index.js'
+
+export const options = {
+  cloud: {
+    distribution: { 'amazon:us:ashburn': { loadZone: 'amazon:us:ashburn', percent: 100 } },
+    apm: [],
+  },
+  thresholds: {},
+  scenarios: {
+    buy_a_pizza: {
+      executor: 'ramping-vus',
+      gracefulStop: '30s',
+      stages: [
+        { target: 20, duration: '10s' },
+        { target: 50, duration: '30s' },
+        { target: 0, duration: '10s' },
+      ],
+      gracefulRampDown: '30s',
+      exec: 'buy_a_pizza',
+    },
+    Imported_HAR: {
+      executor: 'ramping-vus',
+      gracefulStop: '30s',
+      stages: [
+        { target: 20, duration: '1m' },
+        { target: 20, duration: '3m30s' },
+        { target: 0, duration: '1m' },
+      ],
+      gracefulRampDown: '30s',
+      exec: 'imported_HAR',
+    },
+  },
+}
+
+// Scenario: buy_a_pizza (executor: ramping-vus)
+
+export function buy_a_pizza() {
+  let response
+
+  // Automatically added sleep
+  sleep(1)
+}
+
+// Scenario: Imported_HAR (executor: ramping-vus)
+
+export function imported_HAR() {
+  let response
+
+  const vars = {}
+
+  // Login
+  response = http.put(
+    'https://pizza-service.thegamevault.click/api/auth',
+    '{"email":"d@jwt.com","password":"diner"}',
+    {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'content-type': 'application/json',
+        origin: 'https://pizza.thegamevault.click',
+        priority: 'u=1, i',
+        'sec-ch-ua': '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    }
+  )
+
+  if (!check(response, { 'status equals 200': response => response.status.toString() === '200' })) {
+    console.log(response.body);
+    fail('Login was *not* 200');
+  }
+
+  vars['token'] = jsonpath.query(response.json(), '$.token')[0]
+
+  sleep(4)
+
+  // Menu
+  response = http.get('https://pizza-service.thegamevault.click/api/order/menu', {
+    headers: {
+      accept: '*/*',
+      'accept-encoding': 'gzip, deflate, br, zstd',
+      'accept-language': 'en-US,en;q=0.9',
+      authorization: `Bearer ${vars['token']}`,
+      'content-type': 'application/json',
+      origin: 'https://pizza.thegamevault.click',
+      priority: 'u=1, i',
+      'sec-ch-ua': '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-platform': '"Windows"',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-site',
+    },
+  })
+
+  // Franchise
+  response = http.get(
+    'https://pizza-service.thegamevault.click/api/franchise?page=0&limit=20&name=*',
+    {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        authorization: `Bearer ${vars['token']}`,
+        'content-type': 'application/json',
+        origin: 'https://pizza.thegamevault.click',
+        priority: 'u=1, i',
+        'sec-ch-ua': '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    }
+  )
+  sleep(6.8)
+
+  // Me
+  response = http.get('https://pizza-service.thegamevault.click/api/user/me', {
+    headers: {
+      accept: '*/*',
+      'accept-encoding': 'gzip, deflate, br, zstd',
+      'accept-language': 'en-US,en;q=0.9',
+      authorization: `Bearer ${vars['token']}`,
+      'content-type': 'application/json',
+      'if-none-match': 'W/"5d-jqyirYXHQmp5MZjhAPO3OlNtXu8"',
+      origin: 'https://pizza.thegamevault.click',
+      priority: 'u=1, i',
+      'sec-ch-ua': '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-platform': '"Windows"',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-site',
+    },
+  })
+  sleep(1.8)
+
+  // Order
+  response = http.post(
+    'https://pizza-service.thegamevault.click/api/order',
+    '{"items":[{"menuId":2,"description":"Pepperoni","price":0.0042}],"storeId":"1","franchiseId":1}',
+    {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        authorization: `Bearer ${vars['token']}`,
+        'content-type': 'application/json',
+        origin: 'https://pizza.thegamevault.click',
+        priority: 'u=1, i',
+        'sec-ch-ua': '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    }
+  )
+  sleep(2.1)
+
+  if (!check(response, { 'status equals 200': response => response.status.toString() === '200' })) {
+    console.log(response.body);
+    fail('Order was *not* 200');
+  }
+
+  vars['orderJwt'] = jsonpath.query(response.json(), '$.jwt')[0]
+  if (!vars['orderJwt']) {
+    fail('No JWT returned from order response')
+  }
+
+  // Verify
+  response = http.post(
+    'https://pizza-factory.cs329.click/api/order/verify',
+    JSON.stringify({ jwt: vars['orderJwt'] }),
+    {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        authorization: `Bearer ${vars['token']}`,
+        'content-type': 'application/json',
+        origin: 'https://pizza.thegamevault.click',
+        priority: 'u=1, i',
+        'sec-ch-ua': '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'cross-site',
+        'sec-fetch-storage-access': 'active',
+      },
+    }
+  )
+}
